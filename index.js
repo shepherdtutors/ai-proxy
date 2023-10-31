0288ff0e1c7cd49ef598e059e78cc03e03ca6348
Oluwatobi Olufemi <olufemioluwatobi1996@gmail.com>
2023-10-31 17:45:25 +0000

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const httpProxy = require("http-proxy");
+const https = require("https");
+const fs = require("fs");
+
+const app = express();
+const targetIP = "68.183.128.115"; // Your IP
+
+const proxy = httpProxy.createProxyServer({});
+
+app.all("*", (req, res) => {
+  proxy.web(req, res, {
+    target: `http://${targetIP}`,
+  });
+});
+
+// If you have SSL certificates, you can enable this code to run the server with HTTPS.
+// const httpsOptions = {
+//     key: fs.readFileSync('path_to_key.pem'),
+//     cert: fs.readFileSync('path_to_cert.pem')
+// };
+
+// const httpsServer = https.createServer(httpsOptions, app);
+
+// httpsServer.listen(443, () => {
+//     console.log('HTTPS Server running on port 443');
+// });
+
+// For now, using HTTP to test the proxy
+app.listen(8080, () => {
+  console.log("HTTP Proxy server running on port 8080");
+});
